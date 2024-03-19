e6ee9223989de31ca10966576c3196f41523ea93
Qifan Zhang <qifanz98@gmail.com>
2024-03-18 23:56:01 -0700

    test new gitigore; update cdns test script

---
+++ test_infra/host_tmp/dns_send_socket_port.py
@@ -0,0 +1,47 @@
+#!/usr/bin/env python
+# -*- encoding: utf-8 -*-
+
+## python3 dns_send_socket.py [dns_payload] [src_ip] [dst_ip]
+
+import binascii
+import socket
+import sys
+
+from scapy import *
+from scapy.layers import dns
+from scapy.layers.inet import IP, UDP
+from scapy.packet import Raw
+from scapy.sendrecv import send, sr, sr1
+
+src_ip = '172.20.10.1' # test ip
+# src_ip = '172.17.0.6' # example ip
+
+def send_dns_query(src_ip, src_port, dst_ip, dst_port, payload_hex_str):
+	dns_query = Raw(binascii.a2b_hex(payload_hex_str))
+
+	client = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+	client.bind((src_ip, src_port))
+	client.settimeout(5) # add in case of no response
+	
+	try:
+		client.sendto(bytes(dns_query), (dst_ip, dst_port))
+		data, _ = client.recvfrom(65535)
+	except:
+		print('No response for 5 seconds, timed out')
+		return
+
+	# store the data in string format
+	data_str = binascii.b2a_hex(data)
+	print(data_str)
+
+	return
+
+dns_payload = sys.argv[1]
+src_ip = sys.argv[2]
+src_port = int(sys.argv[3])
+dns_ip = sys.argv[4]
+
+# dns_payload_hex_str = "000001000001000000000000046d6372680474787a62036170700000010001"
+dns_payload_hex_str = dns_payload
+
+send_dns_query(src_ip, src_port, dns_ip, 53, dns_payload_hex_str)
--- test_infra/main_cdns.py
+++ test_infra/main_cdns.py
@@ -11,12 +11,15 @@ import time
 import subprocess
 import requests
 import json
+import argparse
+
+parser = argparse.ArgumentParser(description='ResolverFuzz config options')
 
 from input_generation import dns_fuzzer_old as dns_fuzzer
 client = docker.from_env()
 
-total_payload_num = 20 # #payloads (e.g, pair of queries and responses) to be tested for each unit
-unit_size = 40 # temporarily we test the range from 1 to 50
+total_payload_num = 5 # #payloads (e.g, pair of queries and responses) to be tested for each unit
+unit_size = 5 # temporarily we test the range from 1 to 50
 # 20-25 is the best
 # set 20 for stability
 '''
@@ -91,28 +94,28 @@ dnstap_ip_addr = '{}.50.1'.format(ip_addr_range)
 
 # ports for sending queries from attacker containers
 src_port_dict = {
-	'bind9':		110,
-	'unbound':		120,
-	'powerdns':		130,
-	'knot':			140,
-	'maradns':		150,
-	'technitium':	160
+	'bind9':		11000,
+	'unbound':		12000,
+	'powerdns':		13000,
+	'knot':			14000,
+	'maradns':		15000,
+	'technitium':	16000
 }
 
 
 ## file paths
 ## result folder structure: [result_folder_path]/[unit no.]/[round no.]/[dns_sw_name]
-result_folder_path = '/mnt/ssd1/dns_fuzzing_docker/fuzzer_results/cdns_test'
-conf_folder_path = '/home/qifanz/DSP_Lab/dns-fuzzing/dns-docker-images/conf_cdns'
-auth_srv_tmp_path = '/home/qifanz/DSP_Lab/dns-fuzzing/dns-docker-images/auth-srv-batch/auth-srv-tmp'
+result_folder_path = os.path.abspath('./cdns_test_res')
+conf_folder_path = os.path.abspath('../config/conf_cdns')
+auth_srv_tmp_path = os.path.abspath('./auth-srv-tmp')
 
 # dump folders may need to change due to multiple identical containers for one DNS sw
 # for each dns sw container, the dump folder is: 
 # dump_folder_path/[dns_sw_name]/[unit No.]
 # OR
 # (dnstap) dump_folder_path/dnstap/
-dump_folder_path = '/mnt/ssd1/dns_fuzzing_docker/dump'
-attacker_host_tmp_path = '/home/qifanz/DSP_Lab/dns-fuzzing/dns-docker-images/attacker/host_tmp'
+dump_folder_path = os.path.abspath('./dump')
+attacker_host_tmp_path = os.path.abspath('./host_tmp')
 
 
 ## classes for containers
@@ -2192,50 +2195,85 @@ class DNSFuzzerGenerator:
 
 
 ## function declaration and implementation
-def sys_argv_handler():
-	print('sys argv:', sys.argv)
-	global res_time_stamp
-	if '--time_stamp' in sys.argv:
-		res_time_stamp_index = sys.argv.index('--time_stamp')
-		res_time_stamp = float(sys.argv[res_time_stamp_index + 1])
-	else:
-		res_time_stamp = time.time()
+# def sys_argv_handler():
+# 	print('sys argv:', sys.argv)
+# 	global res_time_stamp
+# 	if '--time_stamp' in sys.argv:
+# 		res_time_stamp_index = sys.argv.index('--time_stamp')
+# 		res_time_stamp = float(sys.argv[res_time_stamp_index + 1])
+# 	else:
+# 		res_time_stamp = time.time()
 
-	global result_folder_path
-	result_folder_path = '{res_folder}/{time_stamp}'.format(
-		res_folder = result_folder_path, 
-		time_stamp = res_time_stamp
-	)
+# 	global result_folder_path
+# 	result_folder_path = '{res_folder}/{time_stamp}'.format(
+# 		res_folder = result_folder_path, 
+# 		time_stamp = res_time_stamp
+# 	)
 
-	if '--is_bind9_eval' in sys.argv:
-		global is_bind9_eval
-		is_bind9_eval_index = sys.argv.index('--is_bind9_eval')
-		is_bind9_eval = bool(sys.argv[is_bind9_eval_index + 1])
+# 	if '--is_bind9_eval' in sys.argv:
+# 		global is_bind9_eval
+# 		is_bind9_eval_index = sys.argv.index('--is_bind9_eval')
+# 		is_bind9_eval = bool(sys.argv[is_bind9_eval_index + 1])
 
-	if '--is_unbound_eval' in sys.argv:
-		global is_unbound_eval
-		is_unbound_eval_index = sys.argv.index('--is_unbound_eval')
-		is_unbound_eval = bool(sys.argv[is_unbound_eval_index + 1])
+# 	if '--is_unbound_eval' in sys.argv:
+# 		global is_unbound_eval
+# 		is_unbound_eval_index = sys.argv.index('--is_unbound_eval')
+# 		is_unbound_eval = bool(sys.argv[is_unbound_eval_index + 1])
 
-	if '--is_powerdns_eval' in sys.argv:
-		global is_powerdns_eval
-		is_powerdns_eval_index = sys.argv.index('--is_powerdns_eval')
-		is_powerdns_eval = bool(sys.argv[is_powerdns_eval_index + 1])
+# 	if '--is_powerdns_eval' in sys.argv:
+# 		global is_powerdns_eval
+# 		is_powerdns_eval_index = sys.argv.index('--is_powerdns_eval')
+# 		is_powerdns_eval = bool(sys.argv[is_powerdns_eval_index + 1])
 
-	if '--is_knot_eval' in sys.argv:
-		global is_knot_eval
-		is_knot_eval_index = sys.argv.index('--is_knot_eval')
-		is_knot_eval = bool(sys.argv[is_knot_eval_index + 1])
+# 	if '--is_knot_eval' in sys.argv:
+# 		global is_knot_eval
+# 		is_knot_eval_index = sys.argv.index('--is_knot_eval')
+# 		is_knot_eval = bool(sys.argv[is_knot_eval_index + 1])
 
-	if '--is_maradns_eval' in sys.argv:
-		global is_maradns_eval
-		is_maradns_eval_index = sys.argv.index('--is_maradns_eval')
-		is_maradns_eval = bool(sys.argv[is_maradns_eval_index + 1])
+# 	if '--is_maradns_eval' in sys.argv:
+# 		global is_maradns_eval
+# 		is_maradns_eval_index = sys.argv.index('--is_maradns_eval')
+# 		is_maradns_eval = bool(sys.argv[is_maradns_eval_index + 1])
 	
-	if '--is_technitium_eval' in sys.argv:
-		global is_technitium_eval
-		is_technitium_eval_index = sys.argv.index('--is_technitium_eval')
-		is_technitium_eval = bool(sys.argv[is_technitium_eval_index + 1])
+# 	if '--is_technitium_eval' in sys.argv:
+# 		global is_technitium_eval
+# 		is_technitium_eval_index = sys.argv.index('--is_technitium_eval')
+# 		is_technitium_eval = bool(sys.argv[is_technitium_eval_index + 1])
+# 	return
+
+def sys_argv_handler():
+	## options for disabling testing DNS software
+	parser.add_argument('--disable_bind9', 
+					 	action='store_false')
+	parser.add_argument('--disable_unbound', 
+					 	action='store_false')
+	parser.add_argument('--disable_knot', 
+					 	action='store_false')
+	parser.add_argument('--disable_powerdns', 
+					 	action='store_false')
+	parser.add_argument('--disable_technitium', 
+					 	action='store_false')
+	parser.add_argument('--disable_maradns', 
+					 	action='store_false')
+	
+	## debug mode: if enabled, the program will be single-processed.
+	parser.add_argument('--debug', 
+					 	action='store_true', 
+						help='enable the debug mode so that the program will be single-processed')
+	
+
+	## parse args
+	args = parser.parse_args()
+	is_bind9_eval = args.disable_bind9
+	is_unbound_eval = args.disable_unbound
+	is_knot_eval = args.disable_knot
+	is_powerdns_eval = args.disable_powerdns
+	is_technitium_eval = args.disable_technitium
+	is_maradns_eval = args.disable_maradns
+
+	is_debug = args.debug
+
+
 	return
 
 
